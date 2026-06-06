ef9279a480bef093ae76966012ee414728992daa
lucasWSL <lucas.2610.silva@gmail.com>
2026-06-06 23:44:10 +0100

    Add redirect from /faq to /refs

--- literacia-cosmetica/next.config.ts
+++ literacia-cosmetica/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/faq',
+        destination: '/refs',
+        permanent: true,
+      },
+      {
+        source: '/FAQ',
+        destination: '/refs',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
